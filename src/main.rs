ee35c532de20d3216112e208063cb1561f3d5af5
George Grigalashvili <grigalaa@gmail.com>
2022-11-28 21:35:45 +0000

    Demo generics

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,34 @@
+
+struct S;
+struct GenericVal<T>(T);
+
+impl GenericVal<f32> {} 
+impl GenericVal<S> {} 
+impl<T> GenericVal<T> {}
+
+struct Val {
+    val: f64,
+}
+
+struct GenVal<T> {
+    gen_val: T,
+}
+
+impl Val {
+    fn value(&self) -> &f64 {
+        &self.val
+    }
+}
+
+impl<T> GenVal<T> {
+    fn value(&self) -> &T {
+        &self.gen_val
+    }
+}
+
 fn main() {
-    println!("Hello, world!");
+    let x = Val { val: 3.0 };
+    let y = GenVal { gen_val: 3i32 };
+
+    println!("{}, {}", x.value(), y.value());
 }
